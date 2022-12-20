0296dc8e469dda73d93af6a36b2f38d877b7b67b
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-19 22:55:37 -0300

    add title in blog section

--- components/BlogGrid.tsx
+++ components/BlogGrid.tsx
@@ -4,11 +4,19 @@ import Image from 'next/image'
 import BlogCard from './pure/BlogCard'
 const BlogGrid = () => {
   return (
-    <div className={styles.Blog}>
+    <>
+      <div className={styles.titleSection}>
+        <h2>NUESTRO BLOG</h2>
+        <h2>DEL FREE TOUR!</h2>
+        <h1>EL DÍA A DÍA</h1>
+        <hr />
+      </div>
+      <div className={styles.Blog}>
         <BlogCard image={'/assets/1.png'} title={'un titulo'} subtitle={'subtitulo'} date={'19 dic 22'}/>
         <BlogCard image={'/assets/1.png'} title={'un titulo'} subtitle={'subtitulo'} date={'19 dic 22'}/>
         <BlogCard image={'/assets/1.png'} title={'un titulo'} subtitle={'subtitulo'} date={'19 dic 22'}/>
-    </div>
+      </div>
+    </>
   )
 }
 
