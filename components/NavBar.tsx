5a0c41976959f8c7ead2b74e830772c53a6f7682
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-20 17:36:44 -0300

    add NavBarAnimation

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,21 +4,40 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 export const NavBar = () => {
+  const [active, setActive] = useState("navBarMenu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "navBarMenu") {
+      setActive("navBarMenu nav__active");
+    } else setActive("navBarMenu");
+
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
   return (
-    <div className={styles.navBar}>
-      <div className={styles.navBarItems}>
-        <Link href={'/'}>Inicio</Link>
-        <Link href={'/'}>Destinos</Link>
-        <Link href={'/'}>Novedades</Link>
+    <nav className={styles.navBar}>
+      <Image src={'/assets/logo.png'} alt={''} height={45} width={350} className={styles.navBarImage}/>
+      <div className={active}>
+          <Link href={'/'}>
+            <h3>INICIO</h3>
+            <hr />
+          </Link>
+          <Link href={'/'}>
+            <h3>DESTINOS</h3>
+            <hr />
+          </Link>
+          <Link href={'/'}>
+            <h3>BLOG</h3>
+            <hr />
+          </Link>
       </div>
-      <div>
-        <Image src={'/assets/logo.png'} alt={''} height={45} width={350} className={styles.navBarImage}/>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
       </div>
-      <div className={styles.navBarMenu}>
-        <div className='line1'></div>
-        <div className='line2'></div>
-        <div className='line3'></div>
-      </div>
-    </div>
+    </nav>
   )
 }
