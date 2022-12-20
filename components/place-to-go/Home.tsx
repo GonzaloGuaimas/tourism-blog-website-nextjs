18877c58e78073c4af1b2a252bdba632bad39e95
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-20 20:09:02 -0300

    add Home section

---
+++ components/place-to-go/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from '../../styles/Places.module.css'
+import Image from 'next/image'
+
+const Home = ({placeName, description}: {placeName: string, description: string}) => {
+  return (
+    <div className={styles.Home} id={'home'}>
+        <div className={styles.HomeInfo}>
+            <h2>{placeName}</h2>
+            <hr />
+            <p>{description}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
