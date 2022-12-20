ecafe15733d804d32839f87cd21b583d10ae92a0
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-20 15:30:14 -0300

    add NavBarIcon

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export const NavBar = () => {
   return (
     <div className={styles.navBar}>
-      <Link href={'/'}>Inicio</Link>
-      <Link href={'/'}>Destinos</Link>
-      <Link href={'/'}>Novedades</Link>
+      <div className={styles.navBarItems}>
+        <Link href={'/'}>Inicio</Link>
+        <Link href={'/'}>Destinos</Link>
+        <Link href={'/'}>Novedades</Link>
+      </div>
+      <div>
+        <Image src={'/assets/logo.png'} alt={''} height={45} width={350} className={styles.navBarImage}/>
+      </div>
+      <div className={styles.navBarMenu}>
+        <div className='line1'></div>
+        <div className='line2'></div>
+        <div className='line3'></div>
+      </div>
     </div>
   )
 }
