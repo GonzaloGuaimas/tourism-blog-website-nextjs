654f5d885880be4cb3abe3ab849982d543554ffb
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-21 11:41:36 -0300

    fix top delimiter

--- components/Delimiter.tsx
+++ components/Delimiter.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const Delimiter = ({image, title}: {image: string, title: string}) => {
   return (
     <div className={styles.Delimiter}>
-        <div style={{height: 50, overflow: 'hidden'}}><svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: '100%', width:'100%'}}><path d="M-29.62,118.92 C149.99,150.00 315.18,48.86 517.21,118.92 L500.00,150.00 L0.00,150.00 Z" style={{ stroke: 'none', fill: '#F19705'}}></path></svg></div>
+        <div style={{height: 50, overflow: 'hidden', transform: 'translateY(2px)'}}><svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: '100%', width:'100%'}}><path d="M-29.62,118.92 C149.99,150.00 315.18,48.86 517.21,118.92 L500.00,150.00 L0.00,150.00 Z" style={{ stroke: 'none', fill: '#F19705'}}></path></svg></div>
         <div className={styles.DelimiterContent}>
             <h2>{title}</h2>
             <Image src={image} alt={''} height={180} width={170} className={styles.DelimiterImage}/>
