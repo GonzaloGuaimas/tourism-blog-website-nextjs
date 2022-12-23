b3322130037b139aaf788b7b4a6d63d927f19aa6
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-23 16:02:12 -0300

    fix blog style card

--- components/pure/BlogCard.tsx
+++ components/pure/BlogCard.tsx
@@ -9,9 +9,16 @@ const BlogCard = ({ image, title, subtitle, date }: {image: string, title: strin
         
         <div className={styles.BlogInfo}>
             <h2>{title}</h2>
-            <hr />
             <h3>{subtitle}</h3>
-            <p>{date}</p>
+            <div>
+              <p>{date}</p>
+              <div>
+                <p>BarilocheFreeTour</p>
+                <Image src={image} alt={''} height={250} width={250} className={styles.AutorImage}/>
+              </div>
+            </div>
+            <hr />
+            
         </div>
     </div>
   )
