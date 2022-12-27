3adac7df7f81b0636aa6027b2cb695bc9dcb7939
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-27 12:15:42 -0300

    add Header

---
+++ components/admin/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from '../../styles/Admin.module.css'
+
+const Header = ({ tourName, tourlogo }: {tourName: string, tourlogo: string}) => {
+  return (
+    <div className={styles.Header}>
+        <div>
+
+          <h2>Bienvenido <strong>{tourName}</strong></h2>
+          <hr />
+          <h3>A su administrador de Contenido</h3>
+        </div>
+        <Image src={tourlogo} alt={''} height={250} width={250}/>
+    </div>
+  )
+}
+
+export default Header
