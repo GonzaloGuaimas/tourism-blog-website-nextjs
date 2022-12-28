2ec24066694ce0ed97248481b5b69a3fc79992cb
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-28 00:56:46 -0300

    fix vercel casing problem

---
+++ components/blog/BlogCardLarger.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+const BlogCardLarger = ({ image, title, subtitle, date }: {image: string, title: string, subtitle: string, date: string}) => {
+  return (
+    <div className={`${styles.BlogCard} ${styles.BlogCardLarger}`}>
+        <Image src={image} alt={''} height={1080} width={1080} className={styles.BlogImage}/>
+        
+        <div className={styles.BlogInfo}>
+            <h2>{title}</h2>
+            <h3>{subtitle}</h3>
+            <div>
+              <p>{date}</p>
+              <div>
+                <p>BarilocheFreeTour</p>
+                <Image src={image} alt={''} height={250} width={250} className={styles.AutorImage}/>
+              </div>
+            </div>
+            <hr />
+            
+        </div>
+    </div>
+  )
+}
+
+export default BlogCardLarger
