134be4fcd6db60be60c37d224b65b6254fbcae1f
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-28 01:54:24 -0300

    fix casing error for vercel

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react'
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
-import BlogCardLarger from '../../components/blog/BlogCardLarger'
+import BlogCardLarger from '../../components/blog/blogCardLarger'
 import BlogCard from '../../components/pure/BlogCard'
 import Footer from '../../components/Footer'
 
