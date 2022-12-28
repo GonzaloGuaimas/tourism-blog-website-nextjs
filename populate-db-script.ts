2993a351b13813078e37d0acb0a8a75df67944c3
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-28 12:28:18 -0300

    add tour example

--- populate-db-script.ts
+++ populate-db-script.ts
@@ -10,6 +10,33 @@ const deleteCollections = async () => {
 const populateCollections = async () => {
     createAwards()
     createPosts()
+    createTour()
+}
+const createTour = async () => {
+    const tour1 = new Tour({
+        coverDescription: 'imagen portada',
+            coverImageLink: 'https://res.cloudinary.com/dwh7vwgu4/image/upload/v1672240724/freetours-uploads/py3ym7uuifusddmg3v8m.png',
+            duration: 'duracion',
+            extraInfo:'extr',
+            facebookUser: 'facebook',
+            gallery: [{ title: 'un titulo', imageLink: 'https://res.cloudinary.com/dwh7vwgu4/image/upload/…240703/freetours-uploads/sqzva4i6v6bn2zanyy86.jpg', uploadDate: '2016-05-18T16:00:00Z',
+            userRegisterId: {
+                '_id': '5b6b6959828619572d48a9da'
+              }}],
+            instagramUser: 'instagr',
+            logoImageLink: 'https://res.cloudinary.com/dwh7vwgu4/image/upload/v1672240723/freetours-uploads/v4mapfkpnp5qvd9pswxj.jpg',
+            longerDescription: 'larga enca',
+            meetingPoint: 'pto encuentro',
+            meetingPointLink: 'link google maps',
+            name: 'gonzalo free tour',
+            password: 'contraseñ',
+            route: [{name: 'punto', locationLink: 'linkpunto', imageLink: 'https://res.cloudinary.com/dwh7vwgu4/image/upload/…239978/freetours-uploads/ajpjbgsexysscpbufni2.jpg'},
+            {name: 'punto2', locationLink: 'linkpunto2', imageLink: 'https://res.cloudinary.com/dwh7vwgu4/image/upload/…240629/freetours-uploads/wy4kr9ceytxva49qwwsf.jpg'}],
+            schedules: 'horarios',
+            shortDescription: 'corta encabezado',
+            whatsAppNumber: '92013'
+    })
+    Tour.create(tour1)
 }
 const createAwards = async () => {
     const award1 = new Award({
