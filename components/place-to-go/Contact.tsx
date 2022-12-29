9e5baffce0406a62b9879f6ca7c580e0f0f5893c
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-29 02:36:24 -0300

    change contact text

--- components/place-to-go/Contact.tsx
+++ components/place-to-go/Contact.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import styles from '../../styles/Places.module.css'
-import { IoLogoWhatsapp } from 'react-icons/io5'
 
 const Contact = () => {
   return (
     <div className={styles.Contact}>
-        <IoLogoWhatsapp className={styles.AboutCardIcon} style={{ fontSize: '5rem'}}/>
-        <button className='Button'><span>Pedí +Info por WhatsApp</span> </button>
-        <p>Queres participar de este Free Tour? Hace click acá</p>
+        <button className='Button' style={{ width: '100%'}}><span>Reservar</span> </button>
+        <p>Reservá tu lugar vía WhatsApp!</p>
     </div>
   )
 }
