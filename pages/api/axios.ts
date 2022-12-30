ff77b80cab25188283c758e281c89d1e629a54d8
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-29 23:49:19 -0300

    fix auth api and axios

--- pages/api/axios.ts
+++ pages/api/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios'
 
 const axiosConfig: AxiosRequestConfig = {
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://www.freetourargentina.com/api',
     headers: {
       'Content-type': 'application/json',
       Accept: 'application/json'
