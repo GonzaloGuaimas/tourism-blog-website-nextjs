079b2b32a89a4fccb26d473dc2e14bd9cf14b91a
gonzaloguaimas <gonzaloguaimas@gmail.com>
2023-01-02 15:34:59 -0300

    test award endpoint without mongo

--- src/pages/api/awards/index.ts
+++ src/pages/api/awards/index.ts
@@ -1,30 +1,38 @@
 // import { NextApiRequest, NextApiResponse } from 'next'
-import dbConnect from '../../../lib/dbConnect'
-import Award from '../../../models/Award'
+// import dbConnect from '../../../lib/dbConnect'
+// import Award from '../../../models/Award'
 
 export default async function (req: any, res: any) {
-    const { method, body } = req
-
-    await dbConnect() 
-    switch (method) {
+    const { method } = req
+    switch (method){
         case 'GET':
             try {
-                const awards = await Award.find()
-                return res.status(200).send({awards})
-            } catch (error) {
-                res.status(400).json({ success: false, error })
+                return res.status(200).send({ message: 'hola'})
+            } catch(error){
+                res.status(400).json({ success: false })
             }
-            break
-        case 'POST': 
-            try {
-                const award = await Award.create(body)
-                return res.status(200).json({award})
-            } catch (error) {
-                res.status(400).json({success: false, error})
-            }
-            break
-        default:
-            res.status(400).json({success: false})
             break
     }
+    // await dbConnect() 
+    // switch (method) {
+    //     case 'GET':
+    //         try {
+    //             const awards = await Award.find()
+    //             return res.status(200).send({awards})
+    //         } catch (error) {
+    //             res.status(400).json({ success: false, error })
+    //         }
+    //         break
+    //     case 'POST': 
+    //         try {
+    //             const award = await Award.create(body)
+    //             return res.status(200).json({award})
+    //         } catch (error) {
+    //             res.status(400).json({success: false, error})
+    //         }
+    //         break
+    //     default:
+    //         res.status(400).json({success: false})
+    //         break
+    // }
 }
