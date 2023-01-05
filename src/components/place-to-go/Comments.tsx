a6c148a249a967bbade1564a648dfc0d67fb0e07
gonzaloguaimas <gonzaloguaimas@gmail.com>
2023-01-05 19:31:18 -0300

    add Swiper slider for comments

--- src/components/place-to-go/Comments.tsx
+++ src/components/place-to-go/Comments.tsx
@@ -3,24 +3,37 @@ import styles from '../../../styles/Places.module.css'
 import { IComment, ITour } from '../../models/Tour'
 import CommentCard from '../pure/place-to-go/CommentCard'
 import CommentDialog from '../pure/place-to-go/CommentDialog'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import useScreenWidth from '../../hooks/useScreenWidth'
 
 const Comments = ({ tour }: { tour: ITour}) => {
   const [showCommentDialog, setShowCommentDialog] = useState(false)
+  // const [width, setWidth] = useState<number>(window.innerWidth)
+  // console.log(width)
+  const { isMobile } = useScreenWidth() 
   return (
     <>
         <div className={'titleSection'} id={'blog'}>
           <h3>Opiniones del Tour</h3>
           <hr />
         </div>
-        <div className={styles.Comments}>
+        <Swiper slidesPerView={isMobile ? 1 : 3} pagination={{clickable: true}} modules={[Pagination]} className="mySwiper">
+          <div className={styles.Comments}>
           {
             tour?.comments.map((comment: IComment) => {
               return(
-                <CommentCard key={comment?.name} name={comment?.name} date={comment?.date.toString().split('T')[0]} country={comment?.country} comment={comment?.comment}/>
+                <SwiperSlide key={comment?.name}>
+                <CommentCard  name={comment?.name} date={comment?.date.toString().split('T')[0]} country={comment?.country} comment={comment?.comment}/>
+                </SwiperSlide>
               )
             })
           }
-        </div>
+          </div>
+        </Swiper>
         <button className='Button' onClick={() => setShowCommentDialog(true)}><span>Agregar Opinión</span></button>
         <CommentDialog tour={tour} showCommentDialog={showCommentDialog} hideCommentDialog={() => setShowCommentDialog(false)}/>
     </>
