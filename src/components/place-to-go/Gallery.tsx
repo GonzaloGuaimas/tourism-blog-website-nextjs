00e5595ffc11aa67f69ddaffdee25266a47e7127
gonzaloguaimas <gonzaloguaimas@gmail.com>
2023-01-05 19:43:40 -0300

    change Gallery Slider

--- src/components/place-to-go/Gallery.tsx
+++ src/components/place-to-go/Gallery.tsx
@@ -2,21 +2,28 @@ import React from 'react'
 import styles from '../../../styles/Places.module.css'
 import Image from 'next/image'
 import { IGallery, ITour } from '../../models/Tour'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay, Pagination, Navigation } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 
 const Gallery = ({tour}: {tour:  ITour}) => {
   return (
     <>
       <div style={{height: 70, overflow: 'hidden', width: '100%', zIndex:'10', transform: 'translateY(10px)'}} ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: '100%', width:'100%'}}><path d="M-27.93,7.42 C149.99,150.00 271.49,-49.98 500.00,49.98 L500.00,0.00 L0.00,0.00 Z" style={{ stroke: 'none', fill: '#fff'}}></path></svg></div>  
       <div className={styles.Gallery} id='gallery'>
-        <div className={styles.GallerySlider}>
-            {
+        <Swiper spaceBetween={30} centeredSlides={true} autoplay={{ delay: 1500, disableOnInteraction: false, }} pagination={{ clickable: true, }} navigation={true} modules={[Autoplay, Pagination, Navigation]} className="mySwiper">
+          {
               tour?.gallery?.map((gallery: IGallery) => {
                 return( 
-                  <Image key={gallery.imageLink} src={gallery.imageLink} alt={''} height={1080} width={1080} className={styles.BlogImage}/> 
+                  <SwiperSlide key={gallery.imageLink}>
+                    <Image  src={gallery.imageLink} alt={''} height={1080} width={1080} className={styles.BlogImage}/> 
+                  </SwiperSlide>
                 )
               })
             }
-        </div>
+        </Swiper>
       </div>
       <div style={{height: 70, overflow: 'hidden', width: '100%', transform: 'translateY(-120px)'}}><svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: '100%', width:'100%'}}><path d="M-29.62,118.92 C149.99,150.00 315.18,48.86 517.21,118.92 L500.00,150.00 L0.00,150.00 Z" style={{ stroke: 'none', fill: '#fff'}}></path></svg></div>
     </>
