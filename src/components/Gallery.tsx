997427c32bd6385ea6d6064aed4ce513aa3a30b6
gonzaloguaimas <gonzaloguaimas@gmail.com>
2023-01-07 12:34:58 -0300

    change gallery img styles

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -14,6 +14,14 @@ const Gallery = ({ tours }: { tours: ITour[]}) => {
                     <div key={tour?.name} className={styles.GalleryElement}>
                         <Link href={'tour/'+tour?.name}>
                             <Image src={tour?.gallery[0].imageLink} alt={''} height={500} width={500} className={styles.DestinationImage}/>
+                            <div className={styles.TourInfo}>
+                                <h5>{tour?.name}</h5>
+                                <hr />
+                                <p>{tour?.gallery[0].title}</p>
+                            </div>
+                            <div className={styles.TourLogo}>
+                                <Image src={tour?.logoImageLink} alt={''} height={250} width={250}/>
+                            </div>
                         </Link>
                     </div>
                 )
