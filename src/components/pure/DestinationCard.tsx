dd8b088724f40634f36ba7f5401138f6865d943d
gonzaloguaimas <gonzaloguaimas@gmail.com>
2023-01-07 12:12:32 -0300

    change destination card style

--- src/components/pure/DestinationCard.tsx
+++ src/components/pure/DestinationCard.tsx
@@ -4,7 +4,7 @@ import styles from '../../../styles/Home.module.css'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 
-const DestinationCard = ({ image, title, description }: {image: string, title: string, description: string}) => {
+const DestinationCard = ({ image, logo, title, description }: {image: string, logo: string, title: string, description: string}) => {
   const router = useRouter()
   return (
     <div className={styles.DestinationCard}>
@@ -12,7 +12,16 @@ const DestinationCard = ({ image, title, description }: {image: string, title: s
         <div className={styles.DestinationCardInfo}>
             <h3>{title}</h3>
             <p>{description}</p>
-            <button onClick={() => {router.push(`tour/${title}`)}}>Descubrir Destino</button>
+            <button onClick={() => {router.push(`tour/${title}`)}}>Más Info del Tour</button>
+        </div>
+        <div className={styles.TourInfo}>
+          <p>{title}</p>
+        </div>
+        <div className={styles.BookingNow}>
+          <p>Reservá Ahora</p>
+        </div>
+        <div className={styles.TourImage}>
+          <Image src={logo} alt={''} height={250} width={250}/>
         </div>
     </div>
   )
