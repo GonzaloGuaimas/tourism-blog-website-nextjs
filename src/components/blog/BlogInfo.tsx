3658b4a51244b67aee095b7e2e3fac3253c8d6af
gonzaloguaimas <gonzaloguaimas@gmail.com>
2023-01-07 16:18:28 -0300

    add fetch logo

--- src/components/blog/BlogInfo.tsx
+++ src/components/blog/BlogInfo.tsx
@@ -3,12 +3,17 @@ import styles from '../../../styles/Home.module.css'
 
 import Image from 'next/image'
 import { IPost } from '../../models/Post'
+import { useQuery } from 'react-query'
+import { getTourLogo } from '../../services/tours/getTourLogo'
+import { getTours } from '../../services/tours/getTours'
 
 export const BlogInfo = ({ post }: { post: IPost}) => {
+  const tourQuery = useQuery('tours', getTours)
+  const logo = getTourLogo(tourQuery, post.tourName)
   return (
     <div className={styles.BlogDetailDate}>
         <div>
-            <Image src={'/assets/blogExample/1.jpg'} alt={''} height={250} width={250} className={styles.AutorImage}/>
+            <Image src={logo} alt={''} height={250} width={250} className={styles.AutorImage}/>
             <h3>{post?.tourName}</h3>
         </div>
         <hr />
