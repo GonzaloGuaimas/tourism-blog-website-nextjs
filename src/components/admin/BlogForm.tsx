3242a361e6c04c5081a1f5c8921dcd0588a5c6fb
gonzaloguaimas <gonzaloguaimas@gmail.com>
2023-01-10 23:51:28 -0300

    fix blog form style

--- src/components/admin/BlogForm.tsx
+++ src/components/admin/BlogForm.tsx
@@ -13,6 +13,7 @@ import useNewPosts from '../../hooks/admin/useNewPosts'
 import { ITour } from '../../models/Tour'
 import useDeletePost from '../../hooks/admin/useDeletePost'
 import { Toast } from 'primereact/toast'
+import { Accordion, AccordionTab } from 'primereact/accordion'
 
 const BlogForm = ({ postsData, tour }: { postsData: IPost[], tour: ITour}) => {
     const defaultValues = {
@@ -33,77 +34,84 @@ const BlogForm = ({ postsData, tour }: { postsData: IPost[], tour: ITour}) => {
   return (
     <>
         <div className={styles.MainInfo}>
-            <h2>Nueva Publicación</h2>
-            <form onSubmit={handleSubmit(onSubmit)} className="p-fluid">
-                <div className={styles.ImageField}>
-                    <label htmlFor="imageLink">
-                        Seleccionar Imagen
-                        <input id='imageLink' type="file" accept=".png, .jpg, .jpeg" name='file' onChange={handleOnChange}/>
-                    </label>
-                    <Image src={image || '/assets/emptyImage.png'} alt={''} height={500} width={500}/>
-                </div>
+            <Accordion activeIndex={0}>
+                <AccordionTab header="Publicaciones">
+                    <div className={styles.DataTableField}>
+                        <div>
+                            <h2>Publicaciones</h2>
+                        </div>
+                        <DataTable value={posts} size="small" responsiveLayout="scroll">
+                            {/* <Column field="uploadDate" header="Fecha Subida"></Column> */}
+                            {/* <Column field="date" header="Fecha"></Column> */}
+                            <Column field="title" header="Título"></Column>
+                            <Column field="subtitle" header="Subtítulo"></Column>
+                            <Column field="imageLink" header="Imagen" 
+                            body={(rowData) => (<Image src={rowData.imageLink} alt={''} height={500} width={500}/>)}/>
+                            <Column body={(rowData) => (<Button className={styles.RowButton} type='button' icon="pi pi-trash" onClick={() => {
+                                mutation.mutate(rowData)
+                                removeItem(rowData)
+                            }} />)}/>
+                        </DataTable>
+                    </div>
+                </AccordionTab>
+                <AccordionTab header="Nueva Publicación">
+                    <form onSubmit={handleSubmit(onSubmit)} className="p-fluid">
+                        <div className={styles.ImageField}>
+                            <label htmlFor="imageLink">
+                                Seleccionar Imagen
+                                <input id='imageLink' type="file" accept=".png, .jpg, .jpeg" name='file' onChange={handleOnChange}/>
+                            </label>
+                            <Image src={image || '/assets/emptyImage.png'} alt={''} height={500} width={500}/>
+                        </div>
 
-                <div className={styles.field}>
-                    <span className="p-float-label">
-                        <Controller name="title" control={control} rules={{ required: 'Ingrese Título de la Publicación.' }} render={({ field, fieldState }) => (
-                            <InputText id={field.name} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
-                        )} />
-                        <label htmlFor="title" className={classNames({ 'p-error': errors.title })}>Título de la Publicación*</label>
-                    </span>
-                    {errors['title'] && <small className="p-error">{errors['title'].message}</small>}
-                </div>
+                        <div className={styles.field}>
+                            <span className="p-float-label">
+                                <Controller name="title" control={control} rules={{ required: 'Ingrese Título de la Publicación.' }} render={({ field, fieldState }) => (
+                                    <InputText id={field.name} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                )} />
+                                <label htmlFor="title" className={classNames({ 'p-error': errors.title })}>Título de la Publicación*</label>
+                            </span>
+                            {errors['title'] && <small className="p-error">{errors['title'].message}</small>}
+                        </div>
 
-                <div className={styles.field}>
-                    <span className="p-float-label">
-                        <Controller name="subtitle" control={control} rules={{ required: 'Ingrese Subtítulo de la Publicación.' }} render={({ field, fieldState }) => (
-                            <InputText id={field.name} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
-                        )} />
-                        <label htmlFor="subtitle" className={classNames({ 'p-error': errors.subtitle })}>Subtítulo de la Publicación*</label>
-                    </span>
-                    {errors['subtitle'] && <small className="p-error">{errors['subtitle'].message}</small>}
-                </div>
+                        <div className={styles.field}>
+                            <span className="p-float-label">
+                                <Controller name="subtitle" control={control} rules={{ required: 'Ingrese Subtítulo de la Publicación.' }} render={({ field, fieldState }) => (
+                                    <InputText id={field.name} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                )} />
+                                <label htmlFor="subtitle" className={classNames({ 'p-error': errors.subtitle })}>Subtítulo de la Publicación*</label>
+                            </span>
+                            {errors['subtitle'] && <small className="p-error">{errors['subtitle'].message}</small>}
+                        </div>
 
-                <h2>Contenido de la publicación</h2>
-                <Button className={styles.TableButton} type='button' label="Nuevo Párrafo" onClick={() => setShowContentDialog(true)}/>
-                <h2>Vista Previa</h2>
-                <div className={styles.BlogPreview}>
-                    {
-                        contents.map((content: IContent) => {
-                            return (
-                                <div key={content.title} className={styles.BlogContent}>
-                                    <div>
-                                        <h2>{content.title}</h2>
-                                        <p>{content.paragraph}</p>
-                                    </div>
-                                    {content.imageLink !== undefined ? <Image src={content.imageLink} alt={''} height={1080} width={1080} className={styles.BlogImage}/> : null}
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-               
+                        <h2>Contenido de la publicación</h2>
+                        <Button className={styles.TableButton} type='button' label="Nuevo Párrafo" onClick={() => setShowContentDialog(true)}/>
+                        <h2>Vista Previa</h2>
+                        <div className={styles.BlogPreview}>
+                            {
+                                contents.map((content: IContent) => {
+                                    return (
+                                        <div key={content.title} className={styles.BlogContent}>
+                                            <div>
+                                                <h2>{content.title}</h2>
+                                                <p>{content.paragraph}</p>
+                                            </div>
+                                            {content.imageLink !== undefined ? <Image src={content.imageLink} alt={''} height={1080} width={1080} className={styles.BlogImage}/> : null}
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    
 
-                <Button type="submit" loading={loading} label="Guardar" className="mt-2" />
-                <p>Después de realizar los cambios, Guardalos!</p>
-            </form>
+                        <Button type="submit" loading={loading} label="Guardar" className="mt-2" />
+                        <p>Después de realizar los cambios, Guardalos!</p>
+                    </form>
+                </AccordionTab>
+            </Accordion>
             
-            <div className={styles.DataTableField}>
-                <div>
-                    <h2>Publicaciones</h2>
-                </div>
-                <DataTable value={posts} size="small" responsiveLayout="scroll">
-                    {/* <Column field="uploadDate" header="Fecha Subida"></Column> */}
-                    {/* <Column field="date" header="Fecha"></Column> */}
-                    <Column field="title" header="Título"></Column>
-                    <Column field="subtitle" header="Subtítulo"></Column>
-                    <Column field="imageLink" header="Imagen" 
-                    body={(rowData) => (<Image src={rowData.imageLink} alt={''} height={500} width={500}/>)}/>
-                    <Column body={(rowData) => (<Button className={styles.RowButton} type='button' icon="pi pi-trash" onClick={() => {
-                        mutation.mutate(rowData)
-                        removeItem(rowData)
-                    }} />)}/>
-                </DataTable>
-            </div>
+            
+
         </div>
         <ContentDialog showContentDialog={showContentDialog} hideContentDialog={() => setShowContentDialog(false)} setContents={setContents} toast={toast}/>
         <Toast ref={toast} position='bottom-right'/>
